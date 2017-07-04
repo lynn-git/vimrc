7bbed39087f77f3aaf3e88a5e078f68fc8c166ef
zhongshuai <zhongshuai@baidu.com>
2017-07-04 20:55:38 +0800

    Add install.sh
    Install script for vi
    
    Signed-off-by: zhongshuai <zhongshuai@baidu.com>

---
+++ tools/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install vim-gnome
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
